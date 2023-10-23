31ad7aa56450b90f1c508029e6b76cdaa498e9f4
Anselmo <ansinftec17@gmail.com>
2023-10-23 11:30:33 -0300

    FEAT:Adicionando configuração na navegação das paginas

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,36 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './pages/home';
 import { Password } from './pages/password';
-
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 
-export function Routes(){
-    return(
+export function Routes() {
+    return (
         <Tab.Navigator>
             <Tab.Screen
-            name='home'
-            component={Home}
-            options={{
-                headerShown:false
-            }}
+                name='home'
+                component={Home}
+                options={{
+                    tabBarShowLabelLabel:false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused, size, color }) => {
+                        const nameCurrent = focused ? 'home' : 'home-outline';
+                        return <Ionicons size={size} color={color} name={nameCurrent} />
+                    }
+                }}
             />
             <Tab.Screen
-            name='password'
-            component={Password}
-            options={{
-                headerShown:false
-            }}
+                name='password'
+                component={Password}
+                options={{
+                    tabBarShowLabelLabel:false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused, size, color }) => {
+                        const nameCurrent = focused ? 'lock-closed' : 'lock-closed-outline';
+                        return <Ionicons size={size} color={color} name={nameCurrent} />
+                    }
+                }}
             />
         </Tab.Navigator>
     )
