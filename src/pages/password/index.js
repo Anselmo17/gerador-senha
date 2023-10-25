9859dc09b88c02f0ac20a2b8836abb386b364a1f
Anselmo <ansinftec17@gmail.com>
2023-10-25 07:31:37 -0300

    FEAT:Adicionando chamada de serviço para adicionar senhas

--- src/pages/password/index.js
+++ src/pages/password/index.js
@@ -1,15 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import useStorage from '../../hooks/userStorage';
 
+export function Password() {
+    const [passwords, setPasswords] = useState([]);
+    const focused = useIsFocused();
+    const { getItem } = useStorage();
 
-export function Password(){
+    useEffect(() => {
 
-    return(
-        <View>
-            <Text>Minhas Senhas</Text>
-        </View>
+        async function loadPassword() {
+            const passwordsCurrent = await getItem('Gpass');
+            console.log('----- retorno dados ---------', passwordsCurrent);
+            setPasswords(passwordsCurrent);
+        }
+
+        loadPassword();
+    }, [focused]);
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Minhas Senhas</Text>
+            </View>
+
+            <View style={styles.content}>
+                <FlatList
+                style={{flex:1, paddingTop:14}}
+                data={passwords}
+                keyExtractor={({item})=>String(item)}
+                renderItem={({item})=><Text>{item}</Text>}
+                />
+            </View>
+        </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
-
+    header: {
+        backgroundColor: '#392de9',
+        paddingTop: 58,
+        paddingBottom: 14,
+        paddingLeft: 14,
+        paddingRight: 14,
+    },
+    title: {
+        fontSize: 18,
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    content:{
+        flex:1,
+        paddingLeft: 14,
+        paddingRight: 14,
+    }
 });
